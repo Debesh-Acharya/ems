70c63464b267e183cd6ca84ede865d04568cdfb6
Debesh-Acharya <debeshacharya30@gmail.com>
2024-10-24 15:49:00 +0530

    logout

--- ems/src/components/TaskList/TaskList.jsx
+++ ems/src/components/TaskList/TaskList.jsx
@@ -7,18 +7,18 @@ import FailedTask from './FailedTask'
 const TaskList = ({data}) => {
   return (
     <div id='tasklist' className='h-[55%] overflow-x-auto flex items-center justify-start gap-5 flex-nowrap w-full py-5 mt-10'>
-        {data.tasks.map((e)=>{
+        {data.tasks.map((e, idx)=>{
             if(e.active){
-                return <AcceptTask/>
+                return <AcceptTask key={idx}/>
             }
             if(e.newTask){
-                return <NewTask/>
+                return <NewTask key={idx}/>
             }
             if(e.completed){
-                return <CompleteTask/>
+                return <CompleteTask key={idx}/>
             }
             if(e.failed){
-                return <FailedTask/>
+                return <FailedTask key={idx}/>
             }
         })}
     </div>
